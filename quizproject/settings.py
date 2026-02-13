263b6baad5b8d40f557dc0e2ca4b1dacf164e646
Bharath <bharathsanga511@gmail.com>
2026-02-13 12:06:27 +0530

    Add Render URL to ALLOWED_HOSTS

--- quizproject/settings.py
+++ quizproject/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^t)+cgm&$ha9!b37&qi*&b4!&*duh$g)i%1yw%_u^*co7=q)5v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-quiz-app-1-gm55.onrender.com']
+
 
 
 # Application definition
